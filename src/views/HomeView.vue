f61c9e8559bb973921a5524865579846443a4593
Daun <daunje0@gmail.com>
2024-03-08 02:21:47 +0900

    router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <h2>Home View</h2>
-    <p>{{ $route }}</p>
+    <p>{{ $route.path }}</p>
+    <p>{{ $route.name }}</p>
+    <button class="btn btn-primary" @click="movePage">about 페이지 이동</button>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const movePage = () => {
+  router.push('/about')
+}
 
 </script>
 
